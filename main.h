25406991e758766ba37914228a2db0ed6f1bca98
NkamoN <r.nkamo@gmail.com>
2023-12-16 22:31:20 +0200

    RSA FActoring Challenge

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define _GNU_SOURCE
+
+
+/* size_t getline(char **lineptr, size_t *n, FILE *stream); */
+int factorize(char *buffer);
+
+#endif
